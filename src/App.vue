c6ecbb545861352ad104da5ce103799f32c99c51
ArnoHu <huzzh3@outlook.com>
2022-05-24 23:46:45 +0800

    完成基本框架的搭建

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <template>
   <el-config-provider namespace="ep">
-    <BaseHeader />
-    <div style="display: flex">
-      <BaseSide />
+    <div style="display: flex" class="main-content">
       <div>
-        <img alt="Vue logo" class="element-plus-logo" src="./assets/logo.png" />
-        <HelloWorld msg="Hello Vue 3.0 + Element Plus + Vite" />
+        <img alt="Vue logo" class="element-plus-logo" src="./assets/sysu-logo.png" />
+        <!-- <HelloWorld msg="Hello Vue 3.0 + Element Plus + Vite" /> -->
+        <Entry entryName="借用雨伞"/>
       </div>
     </div>
   </el-config-provider>
@@ -13,6 +12,7 @@
 
 <style>
 #app {
+  position: absolute;
   text-align: center;
   color: var(--ep-text-color-primary);
 }
---
+++ src/components/Entry.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+defineProps<{ entryName: string }>();
+
+</script>
+
+<template>
+    <div class="entry-body">
+        <img src="../assets/umbrella.png">
+        <h2> {{ entryName }} </h2>
+    </div>
+</template>
